7482e9bb3b3801f9a3d2c0c8705ecfb601be9ffd
Jonathan Hansen <kriah0872@gmail.com>
2020-07-30 18:30:14 -0700

    changed resume link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <Col xs='auto'>
           <a href="https://github.com/jonathanhansen98"><FontAwesomeIcon color='#ebebe3' className='my-2 mx-3 hvr-forward' icon={faGithubSquare} size='3x' /></a>
           <a href="https://www.linkedin.com/in/jon-hansen-dev/"><FontAwesomeIcon color='#ebebe3' className='my-2 mx-3 hvr-forward' icon={faLinkedinIn} size='3x' /></a>
-          <a href="https://drive.google.com/file/d/1nB_vWYoHB0kE-atxyoWhnPc94TnXapv5/view?usp=sharing"><FontAwesomeIcon color='#ebebe3' className='my-2 mx-3 hvr-forward' icon={faFilePdf} size='3x' /></a>
+          <a href="https://drive.google.com/file/d/1utF0VM1j397CGQ6eoEyt0yncbuRUWVH5/view?usp=sharing"><FontAwesomeIcon color='#ebebe3' className='my-2 mx-3 hvr-forward' icon={faFilePdf} size='3x' /></a>
         </Col>
       </Row>
     </div>
